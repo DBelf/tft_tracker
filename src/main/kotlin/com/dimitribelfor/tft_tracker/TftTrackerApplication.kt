2c03512972cc91ee45b0a7027631209dac8be62f
Dimitri Belfor <dimitri.belfor@gmail.com>
2022-10-01 21:16:50 +0200

    initialize new kotlin project

---
+++ src/main/kotlin/com/dimitribelfor/tft_tracker/TftTrackerApplication.kt
@@ -0,0 +1,11 @@
+package com.dimitribelfor.tft_tracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TftTrackerApplication
+
+fun main(args: Array<String>) {
+    runApplication<TftTrackerApplication>(*args)
+}
---
+++ src/test/kotlin/com/dimitribelfor/tft_tracker/TftTrackerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dimitribelfor.tft_tracker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TftTrackerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
